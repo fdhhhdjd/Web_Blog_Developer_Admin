a737eafc858713d4d95cf523595a58c9053712fe
quochao <hao.nguyen@widosoft.com>
2023-08-09 17:48:50 +0700

    #1 Setup page admin

---
+++ src/components/MenuList.vue
@@ -0,0 +1,20 @@
+<script setup>
+import MenutemLink from './MenutemLink.vue';
+
+defineProps({
+  menu: {
+    type: Array,
+    default: () => [],
+  },
+});
+
+const emit = defineEmits(['menuClick']);
+
+const menuClick = (event, item) => {
+  emit('menuClick', event, item);
+};
+</script>
+
+<template>
+  <MenutemLink v-for="(item, index) in menu" :key="index" :item="item" @menu-click="menuClick" />
+</template>
