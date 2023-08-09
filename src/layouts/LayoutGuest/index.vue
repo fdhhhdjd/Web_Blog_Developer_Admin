0451475d4bcd4dfc05bfab5c1b5a66d0c31d8726
Nguyễn Tiến Tài <63393170+fdhhhdjd@users.noreply.github.com>
2023-08-09 14:15:52 +0700

    #1 Setup page admin (#3)
    Co-authored-by: quochao <hao.nguyen@widosoft.com>

---
+++ src/layouts/LayoutGuest/index.vue
@@ -0,0 +1,11 @@
+<script setup>
+console.log('Hello world');
+</script>
+
+<template>
+  <div :class="{}">
+    <div class="bg-gray-50 dark:bg-slate-800 dark:text-slate-100">
+      <slot />
+    </div>
+  </div>
+</template>
